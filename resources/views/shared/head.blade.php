6d16853db6dfd16c19561bec6a96ac5b35cac886
Drozdaaa <krzys6d@gmail.com>
2025-07-09 15:53:03 +0200

    Added MainPage and changed bootstrap theme

---
+++ resources/views/shared/head.blade.php
@@ -0,0 +1,8 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $pageTitle }}</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <script defer src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('js/theme.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(MainController::class)->group(function () {
+    Route::get('/main', 'index')->name('main.index');
 });
